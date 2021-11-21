5efd341931ff4ac7f88b3b280c5917aade816eed
Akhil Reddy <akhilreddymandadi@gmail.com>
2021-11-21 11:33:55 -0800

    Annotation changes

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+var bodyParser = require("body-parser");
 const logger = require('tracer').colorConsole();
 const CONFIG = require("./config/config");
 app.use(cookieParser());
 app.use(express.static('public'));
 app.use(cors({ origin: "*", credentials: true }));
+app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', "*");
   res.setHeader('Access-Control-Allow-Credentials', 'true');
@@ -33,11 +35,19 @@ app.get('/data', async (request, response) => {
       yearTo,
       country
     } = request.query;
-    const query = `SELECT Year as year, ${country} as country, ${country}_Annotation_Title as title, ${country}_Annotation_Text as text FROM ${table} where Year between ${yearFrom} and ${yearTo} and ${country} IS NOT NULL;`;
+    const query = `SELECT Year as year, ${country} as country FROM ${table} where Year between ${yearFrom} and ${yearTo} and ${country} IS NOT NULL;`;
     const rows = await pool.query(query);
-    let resp = rows.map(function (el) {
-      return [el.year, el.country, el.title == null ? "" : el.title, el.text == null ? "" : el.text];
+    let resp = {};
+    resp.rows = rows.map(function (el) {
+      return [el.year, el.country];
     });
+    const anno_query = `SELECT annotation from ANNOTATIONS where table_name="${table}" and country="${country}";`;
+    const anno_rows = await pool.query(anno_query);
+    if (anno_rows.length > 0) {
+      resp.annotation = anno_rows[0]["annotation"];
+    } else {
+      resp.annotation = ""
+    }
     return response.json(resp).status(200);
   } catch (ex) {
     logger.error(JSON.stringify(ex));
@@ -51,12 +61,10 @@ app.post('/annotation', async (request, response) => {
   try {
     const {
       table,
-      year,
       country,
-      title,
       text
     } = request.body;
-    const query = `UPDATE ${table} SET ${country}_Annotation_Title = "${title}", ${country}_Annotation_Text = "${text}" where Year=${year};`;
+    const query = `REPLACE INTO ANNOTATIONS(table_name, country, annotation) values("${table}","${country}","${text}");`
     const rows = await pool.query(query);
     return response.json({ "message": "success" }).status(200);
   } catch (ex) {
